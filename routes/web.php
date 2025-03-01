20d3ca8f8d0f6597bb272b1e1294ece0e83b01f9
Guilhermefariah <Guilhermefaria321@gmail.com>
2025-03-01 14:41:35 -0300

    chamada do middleware pelo construct

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/usuarios', [UsuarioControlador::class, 'index'])
-    ->middleware('primeiro');
+Route::get('/usuarios', [UsuarioControlador::class, 'index']);
